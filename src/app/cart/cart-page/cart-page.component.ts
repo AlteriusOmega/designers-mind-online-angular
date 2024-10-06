405ac16c12233b96588e84625d6d433240f6a317
AlteriusOmega <TheRealSamChaney@gmail.com>
2024-10-06 15:12:13 -0500

    Modified cart-page to use intermediate itemSubscription Subscription variable instead of using items$ directly allowing us to unsubscribe easily to avoid memory leaks

--- src/app/cart/cart-page/cart-page.component.ts
+++ src/app/cart/cart-page/cart-page.component.ts
@@ -1,26 +1,27 @@
-import { Component } from '@angular/core';
+import { Component, OnDestroy, OnInit } from '@angular/core';
 import { CartItem, Product } from 'src/types';
 import { CartService } from '../../shared/cart.service';
+import { Subscription } from 'rxjs';
 
 @Component({
   selector: 'app-cart-page',
   templateUrl: './cart-page.component.html',
   styleUrls: ['./cart-page.component.less'],
 })
-export class CartPageComponent {
+export class CartPageComponent implements OnInit, OnDestroy {
   items: CartItem[] = [];
   total: number = 0;
+  private itemSubscription!: Subscription;
   // Ex: constructor
   constructor(public cartService: CartService) {
     // Ex: rxjs observable with callback function
-    this.cartService.items$.subscribe((newItems) => {
-      // Inside here is the callback function that gets called whenever the observable items$ emits a new value
-      // So, every time items$ observable emits a new items array, we set the local items array this.items to that new one
+    this.itemSubscription = this.cartService.items$.subscribe((newItems) => {
       this.items = newItems;
       this.total = this.cartService.getTotal();
     });
   }
 
+  // Ex: ngOnInit lifecycle hook
   ngOnInit() {
     this.items = this.cartService.getAllItems();
     this.total = this.cartService.getTotal();
@@ -28,5 +29,9 @@ export class CartPageComponent {
 
   removeItem(product: Product) {
     this.cartService.removeFromCart(product);
+  }
+
+  ngOnDestroy(): void {
+    this.itemSubscription.unsubscribe();
   }
 }
